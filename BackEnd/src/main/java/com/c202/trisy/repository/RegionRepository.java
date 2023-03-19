52b1e67d8c1ef7b8b9f6d13e650fabaf3ad58fa3
이충무 <lcm2822@daum.net>
2023-03-19 21:36:48 +0900

    back/feature/getApi
    feature:
    RegionRepository 추가
    ApiController 추가
    -> 지역코드를 가져오려고 시도하나 SERVICE_ACCESS_DENIED_ERROR or SERVICE KEY IS NOT REGISTERED ERROR 발생 다시 서비스키 발급 받아볼 것!

---
+++ BackEnd/src/main/java/com/c202/trisy/repository/RegionRepository.java
@@ -0,0 +1,7 @@
+package com.c202.trisy.repository;
+
+import com.c202.trisy.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, Long> {
+}
