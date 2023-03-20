953ef726956b2d0549729dc433eb0b750f007a5d
박현우 <a40293865@gmail.com>
2023-03-20 15:30:45 +0900

    Feat: kakao api에 마커 찍기

--- FrontEnd/src/pages/recommand/components/KakaoMap.tsx
+++ FrontEnd/src/pages/recommand/components/KakaoMap.tsx
@@ -1,11 +1,18 @@
 import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+
+type MarkerType = {
+  title: string;
+  lat: string;
+  lng: string;
+};
 
 export const KakaoMap = () => {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    window.onload = () => {
+    window.onload = async () => {
       const { kakao } = window;
       const container = mapRef.current;
       const options = {
@@ -13,6 +20,19 @@ export const KakaoMap = () => {
         level: 9,
       };
       const map = new kakao.maps.Map(container, options);
+      axios.get('http://localhost:3003/markers').then((res) => {
+        res.data.forEach((el: MarkerType) => {
+          console.log(el);
+          new kakao.maps.Marker({
+            //마커가 표시 될 지도
+            map: map,
+            //마커가 표시 될 위치
+            position: new kakao.maps.LatLng(el.lat, el.lng),
+            //마커에 hover시 나타날 title
+            title: el.title,
+          });
+        });
+      });
     };
   }, []);
   return <MapContainer ref={mapRef} />;
