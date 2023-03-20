21189878052035b13ec6be79d05fe2c1a2fb8728
이충무 <lcm2822@daum.net>
2023-03-20 12:05:08 +0900

    back/feature/getApi
    feature:
    Theme 엔티티 변경

--- BackEnd/src/main/java/com/c202/trisy/entity/Theme.java
+++ BackEnd/src/main/java/com/c202/trisy/entity/Theme.java
@@ -3,14 +3,20 @@ package com.c202.trisy.entity;
 import lombok.Getter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Getter
 public class Theme {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String mainCategory;
+    private String mainCategoryName;
     private String middleCategory;
+    private String middleCategoryName;
     private String subCategory;
+    private String subCategoryName;
 }
