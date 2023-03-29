bacffafb70f8ccab888b02360348020ca53bbe71
MunTaeHo <ansxogh97@gmail.com>
2023-03-29 15:37:46 +0900

    Feat : BoardAPI

--- BackEnd/src/main/java/com/c202/trisy/board/dto/BoardRequest.java
+++ BackEnd/src/main/java/com/c202/trisy/board/dto/BoardRequest.java
@@ -1,11 +1,13 @@
 package com.c202.trisy.board.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
@@ -21,7 +23,5 @@ public class BoardRequest {
     private Long tourId;
 
     private String thumbnailUrl;
-
-
 
 }
