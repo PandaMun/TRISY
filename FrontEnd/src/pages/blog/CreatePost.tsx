1f7f44bed1be0f69179b0fda8a7f33532334bef5
박현우 <a40293865@gmail.com>
2023-03-30 15:00:18 +0900

    Fix: build error

--- FrontEnd/src/pages/blog/CreatePost.tsx
+++ FrontEnd/src/pages/blog/CreatePost.tsx
@@ -1,90 +1,90 @@
-import styled from 'styled-components';
-import tw from 'twin.macro';
-import React, { useState } from 'react';
-import TextEditor from './components/PostEditor';
-import { Button } from '~/components/Shared/Button';
-import { Link } from 'react-router-dom';
-import { Line } from '~/components/Shared/Line';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { createBoard, getTourListApi } from '~/api/boardApi';
-import { board } from '~/types/sharedTypes';
+// import styled from 'styled-components';
+// import tw from 'twin.macro';
+// import React, { useState } from 'react';
+// import TextEditor from './components/PostEditor';
+// import { Button } from '~/components/Shared/Button';
+// import { Link } from 'react-router-dom';
+// import { Line } from '~/components/Shared/Line';
+// import { useMutation, useQuery } from '@tanstack/react-query';
+// import { createBoard, getTourListApi } from '~/api/boardApi';
+// import { board } from '~/types/sharedTypes';
 
-export const CreatePost = () => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [thumbnailUrl, setThumbnailUrl] = useState('');
+// export const CreatePost = () => {
+//   const [title, setTitle] = useState('');
+//   const [content, setContent] = useState('');
+//   const [thumbnailUrl, setThumbnailUrl] = useState('');
 
-  const { data: tours } = useQuery<board[]>(['tours'], getTourListApi);
-  console.log(tours);
+//   const { data: tours } = useQuery<board[]>(['tours'], getTourListApi);
+//   console.log(tours);
 
-  const handleTextChange = (value: string) => {
-    setContent(value);
-  };
-  const { mutate } = useMutation(createBoard, {
-    onSuccess: (data) => {
-      console.log('Post created:', data);
-      setTitle('');
-      setContent('');
-    },
-    onError: (error) => {
-      console.error('Error creating post:', error);
-    },
-  });
-  const handleCreatePost = () => {
-    mutate({
-      title: title,
-      content: content,
-      tourId: '1',
-      thumbnailUrl,
-    });
-  };
-  return (
-    <S.Box>
-      <S.GridContainer>
-        <S.GridLeft>
-          <Link to='/' className='text-3xl font-bold pl-5'>
-            TRiSY
-          </Link>
-          <Line />
-        </S.GridLeft>
-        <S.GridCenter>
-          <TextEditor value={content} onChange={handleTextChange} setThumbnail={setThumbnailUrl} />
-          <input
-            type='text'
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder='제목을 입력해주세요'
-            className='absolute top-0 mt-12 p-3 py-5 border-b-2 rounded-none text-xl z-40 border-t-2 focus:outline-none focus:border-b-red-100 focus:border-t-[#E9EBEE]'
-          />
-        </S.GridCenter>
-        <S.GridRight>
-          <Button
-            type='button'
-            className='create-post-button bg-black mr-5'
-            text='완료'
-            onClick={handleCreatePost}
-          />
-          <Line />
-        </S.GridRight>
-      </S.GridContainer>
-    </S.Box>
-  );
-};
+//   const handleTextChange = (value: string) => {
+//     setContent(value);
+//   };
+//   const { mutate } = useMutation(createBoard, {
+//     onSuccess: (data) => {
+//       console.log('Post created:', data);
+//       setTitle('');
+//       setContent('');
+//     },
+//     onError: (error) => {
+//       console.error('Error creating post:', error);
+//     },
+//   });
+//   const handleCreatePost = () => {
+//     mutate({
+//       title: title,
+//       content: content,
+//       tourId: '1',
+//       thumbnailUrl,
+//     });
+//   };
+//   return (
+//     <S.Box>
+//       <S.GridContainer>
+//         <S.GridLeft>
+//           <Link to='/' className='text-3xl font-bold pl-5'>
+//             TRiSY
+//           </Link>
+//           <Line />
+//         </S.GridLeft>
+//         <S.GridCenter>
+//           <TextEditor value={content} onChange={handleTextChange} setThumbnail={setThumbnailUrl} />
+//           <input
+//             type='text'
+//             value={title}
+//             onChange={(e) => setTitle(e.target.value)}
+//             placeholder='제목을 입력해주세요'
+//             className='absolute top-0 mt-12 p-3 py-5 border-b-2 rounded-none text-xl z-40 border-t-2 focus:outline-none focus:border-b-red-100 focus:border-t-[#E9EBEE]'
+//           />
+//         </S.GridCenter>
+//         <S.GridRight>
+//           <Button
+//             type='button'
+//             className='create-post-button bg-black mr-5'
+//             text='완료'
+//             onClick={handleCreatePost}
+//           />
+//           <Line />
+//         </S.GridRight>
+//       </S.GridContainer>
+//     </S.Box>
+//   );
+// };
 
-const S = {
-  Box: styled.section`
-    ${tw`fixed top-0 left-0 bottom-0 right-0 z-30 min-h-screen bg-white min-w-[880px] dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200`}
-  `,
-  GridContainer: styled.div`
-    ${tw`grid grid-cols-6 pt-5`}
-  `,
-  GridLeft: styled.div`
-    ${tw`col-span-1 border-white`}
-  `,
-  GridCenter: styled.div`
-    ${tw`col-span-4 relative`}
-  `,
-  GridRight: styled.div`
-    ${tw`col-span-1 text-right`}
-  `,
-};
+// const S = {
+//   Box: styled.section`
+//     ${tw`fixed top-0 left-0 bottom-0 right-0 z-30 min-h-screen bg-white min-w-[880px] dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200`}
+//   `,
+//   GridContainer: styled.div`
+//     ${tw`grid grid-cols-6 pt-5`}
+//   `,
+//   GridLeft: styled.div`
+//     ${tw`col-span-1 border-white`}
+//   `,
+//   GridCenter: styled.div`
+//     ${tw`col-span-4 relative`}
+//   `,
+//   GridRight: styled.div`
+//     ${tw`col-span-1 text-right`}
+//   `,
+// };
--- FrontEnd/src/pages/blog/components/PostEditor.tsx
+++ FrontEnd/src/pages/blog/components/PostEditor.tsx
@@ -1,105 +1,105 @@
-// src/components/TextEditor.tsx
-import { RangeStatic } from 'quill';
-import React, { useMemo, useRef } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+// // src/components/TextEditor.tsx
+// import { RangeStatic } from 'quill';
+// import React, { useMemo, useRef } from 'react';
+// import ReactQuill from 'react-quill';
+// import 'react-quill/dist/quill.snow.css';
 
-interface TextEditorProps {
-  value: string;
-  onChange: (value: string) => void;
-  setThumbnail: (value: string) => void;
-}
+// interface TextEditorProps {
+//   value: string;
+//   onChange: (value: string) => void;
+//   setThumbnail: (value: string) => void;
+// }
 
-const toolbarOptions = [
-  ['image'], // image upload button
-  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+// const toolbarOptions = [
+//   ['image'], // image upload button
+//   [{ color: [] }, { background: [] }], // dropdown with defaults from theme
 
-  ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+//   ['bold', 'italic', 'underline', 'strike'], // toggled buttons
 
-  [{ header: 1 }, { header: 2 }], // custom button values
-  [
-    { list: 'ordered', className: 'custom-ordered-list' },
-    { list: 'bullet', className: 'custom-bullet-list' },
-  ],
-  [
-    { align: '' }, // individual align button
-    { align: 'center' }, // individual align button
-    { align: 'right' }, // individual align button
-    { align: 'justify' }, // individual align button
-  ],
-];
+//   [{ header: 1 }, { header: 2 }], // custom button values
+//   [
+//     { list: 'ordered', className: 'custom-ordered-list' },
+//     { list: 'bullet', className: 'custom-bullet-list' },
+//   ],
+//   [
+//     { align: '' }, // individual align button
+//     { align: 'center' }, // individual align button
+//     { align: 'right' }, // individual align button
+//     { align: 'justify' }, // individual align button
+//   ],
+// ];
 
-const CustomList = ReactQuill.Quill.import('formats/list');
-CustomList.className = 'custom-list';
-ReactQuill.Quill.register(CustomList, true);
+// const CustomList = ReactQuill.Quill.import('formats/list');
+// CustomList.className = 'custom-list';
+// ReactQuill.Quill.register(CustomList, true);
 
-const CustomListItem = ReactQuill.Quill.import('formats/list/item');
-CustomListItem.className = 'custom-list-item';
-ReactQuill.Quill.register(CustomListItem, true);
+// const CustomListItem = ReactQuill.Quill.import('formats/list/item');
+// CustomListItem.className = 'custom-list-item';
+// ReactQuill.Quill.register(CustomListItem, true);
 
-const TextEditor: React.FC<TextEditorProps> = ({ value, onChange }) => {
-  const quillRef = useRef<ReactQuill>(null);
-  const cursorPositionRef = useRef<number | undefined>(undefined);
+// const TextEditor: React.FC<TextEditorProps> = ({ value, onChange }) => {
+//   const quillRef = useRef<ReactQuill>(null);
+//   const cursorPositionRef = useRef<number | undefined>(undefined);
 
-  const onSelectionChange = (range: RangeStatic | null) => {
-    if (range) {
-      cursorPositionRef.current = range.index;
-    }
-  };
-  const imageHandler = () => {
-    const input = document.createElement('input');
-    input.setAttribute('type', 'file');
-    input.setAttribute('accept', 'image/*');
-    input.click();
+//   const onSelectionChange = (range: RangeStatic | null) => {
+//     if (range) {
+//       cursorPositionRef.current = range.index;
+//     }
+//   };
+//   const imageHandler = () => {
+//     const input = document.createElement('input');
+//     input.setAttribute('type', 'file');
+//     input.setAttribute('accept', 'image/*');
+//     input.click();
 
-    const editor = quillRef.current?.getEditor();
+//     const editor = quillRef.current?.getEditor();
 
-    input.onchange = async () => {
-      const file = input.files![0];
-      const reader = new FileReader();
+//     input.onchange = async () => {
+//       const file = input.files![0];
+//       const reader = new FileReader();
 
-      reader.onload = () => {
-        const imageURL = reader.result as string;
-        console.log(imageURL);
-        // TODO: Upload the image to your server and get the image URL
-        // You can replace the imageURL with the actual uploaded image URL after uploading it to your server
+//       reader.onload = () => {
+//         const imageURL = reader.result as string;
+//         console.log(imageURL);
+//         // TODO: Upload the image to your server and get the image URL
+//         // You can replace the imageURL with the actual uploaded image URL after uploading it to your server
 
-        // Insert the image URL to the editor at the last known cursor position
-        if (typeof cursorPositionRef.current === 'number') {
-          editor?.insertEmbed(cursorPositionRef.current, 'image', imageURL);
-        } else {
-          editor?.insertEmbed(editor.getLength(), 'image', imageURL);
-        }
-      };
+//         // Insert the image URL to the editor at the last known cursor position
+//         if (typeof cursorPositionRef.current === 'number') {
+//           editor?.insertEmbed(cursorPositionRef.current, 'image', imageURL);
+//         } else {
+//           editor?.insertEmbed(editor.getLength(), 'image', imageURL);
+//         }
+//       };
 
-      if (file) {
-        reader.readAsDataURL(file);
-      }
-    };
-  };
+//       if (file) {
+//         reader.readAsDataURL(file);
+//       }
+//     };
+//   };
 
-  const modules = useMemo(() => {
-    return {
-      toolbar: {
-        container: toolbarOptions,
-        handlers: {
-          image: imageHandler,
-        },
-      },
-    };
-  }, []);
+//   const modules = useMemo(() => {
+//     return {
+//       toolbar: {
+//         container: toolbarOptions,
+//         handlers: {
+//           image: imageHandler,
+//         },
+//       },
+//     };
+//   }, []);
 
-  return (
-    <ReactQuill
-      ref={quillRef}
-      value={value}
-      onChange={onChange}
-      modules={modules}
-      onChangeSelection={onSelectionChange}
-      className='z-50 border-4'
-      style={{ height: '500px', border: 'none' }}
-    />
-  );
-};
+//   return (
+//     <ReactQuill
+//       ref={quillRef}
+//       value={value}
+//       onChange={onChange}
+//       modules={modules}
+//       onChangeSelection={onSelectionChange}
+//       className='z-50 border-4'
+//       style={{ height: '500px', border: 'none' }}
+//     />
+//   );
+// };
 
-export default TextEditor;
+// export default TextEditor;
