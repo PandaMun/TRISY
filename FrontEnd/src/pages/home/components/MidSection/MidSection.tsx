a8e4dcfb1e1506ee8a9f83e762535e1339e1e34a
박현우 <a40293865@gmail.com>
2023-03-30 11:39:19 +0900

    Feat: main MidCarousel

--- FrontEnd/src/pages/home/components/MidSection/MidSection.tsx
+++ FrontEnd/src/pages/home/components/MidSection/MidSection.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
-import { ImgInfo } from './ImgInfo';
 import { MidCarousel } from './MidCarousel';
 interface Item {
   imageUrl: string;
@@ -20,26 +19,5 @@ const S = {
   Section: styled.div`
     ${tw`min-h-screen mx-auto`}
     width: 70vw;
-  `,
-  SectionTitle: styled.div`
-    ${tw`text-3xl font-bold text-center`}
-  `,
-  SectionContent: styled.div`
-    ${tw`flex justify-around`}
-  `,
-  Card: styled.div`
-    ${tw`max-w-[250px] max-h-[500px] shadow-xl hover:cursor-pointer`}
-  `,
-  CardBox: styled.div`
-    ${tw`w-full h-full`}
-  `,
-  ImageBox: styled.div`
-    ${tw`overflow-hidden`}
-    img {
-      ${tw`transition-all duration-300 hover:scale-125`}
-    }
-  `,
-  ContentBox: styled.div`
-    ${tw`m-5`}
   `,
 };
