0ee396f3325f72bbf0f6ed5df620cf649b4535d2
MunTaeHo <ansxogh97@gmail.com>
2023-04-06 14:10:42 +0900

    Feat : update response dto

--- BackEnd/src/main/java/com/c202/trisy/tour/repository/RegionRepository.java
+++ BackEnd/src/main/java/com/c202/trisy/tour/repository/RegionRepository.java
@@ -4,9 +4,11 @@ import com.c202.trisy.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface RegionRepository extends JpaRepository<Region,Long> {
 
-    Region findRegionBySiName(String name);
+    List<Region> findAllBySiName(String name);
 }
