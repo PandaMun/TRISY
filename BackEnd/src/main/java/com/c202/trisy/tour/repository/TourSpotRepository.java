db1f050f2f81d1981b48a267dad2687f9b205051
MunTaeHo <ansxogh97@gmail.com>
2023-04-06 12:57:13 +0900

    Feat : tourName update API

--- BackEnd/src/main/java/com/c202/trisy/tour/repository/TourSpotRepository.java
+++ BackEnd/src/main/java/com/c202/trisy/tour/repository/TourSpotRepository.java
@@ -1,11 +1,18 @@
 package com.c202.trisy.tour.repository;
 
+import com.c202.trisy.entity.Region;
+import com.c202.trisy.entity.Theme;
 import com.c202.trisy.entity.TourSchedule;
 import com.c202.trisy.entity.TourSpot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TourSpotRepository extends JpaRepository<TourSpot, Long> {
+
+    List<TourSpot> findAllByTheme_IdAndRegion_Id(Long themeId, Long regionId);
 
 }
