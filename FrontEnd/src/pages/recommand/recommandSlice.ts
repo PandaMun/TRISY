d87bb71ece090d22f627240547b337deb6611a0b
박현우 <a40293865@gmail.com>
2023-04-06 21:55:06 +0900

    Fix

--- FrontEnd/src/pages/recommand/recommandSlice.ts
+++ FrontEnd/src/pages/recommand/recommandSlice.ts
@@ -1,8 +1,6 @@
-import { createSlice, current, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import type { RootState } from '~/app/store';
-import { PickList } from './left/PickList';
-
 const initialState = {
   pickList: [],
   recommandList: [],
@@ -24,15 +22,11 @@ const recommandSlice = createSlice({
           break;
         }
       }
-      console.log(state.pickList);
     },
 
     pickPop: (state: any, action: PayloadAction<{ id: number }>) => {
-      console.log('체크픽팝');
       state.pickList = [...state.pickList];
       state.recommandList = [...state.recommandList];
-      console.log(state.pickList);
-      console.log(action.payload);
       for (let i = 0; i < state.pickList.length; i++) {
         if (state.pickList[i].id == action.payload.id) {
           state.recommandList = [...state.recommandList, state.pickList[i]];
@@ -40,14 +34,6 @@ const recommandSlice = createSlice({
           break;
         }
       }
-      // if (state.pickList.includes(action.payload)) {
-      // state.pickList = [...state.pickList.filter((i: any) => i !== action.payload)];
-
-      // }
-
-      console.log('state.pickList');
-      console.log(state.pickList);
-      console.log(state.recommandList);
     },
   },
 });
